5bb57f587d469f32ab848731744cee1192a239ce
UshaRani <r210554@rguktrkv.ac.in>
2026-09-07 10:37:29 +0530

    Add validation route connecting FastAPI to orchestrator

--- server/main.py
+++ server/main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from server.routes.validation import router as validation_router
+app.include_router(validation_router)
 import os
 
 from server.routes import search
